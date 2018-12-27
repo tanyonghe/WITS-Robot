8f86e419b2abda146dd6c9b1102fba92c2d6e369
x3TsunaYH <tyhshana@hotmail.com>
2018-12-27 16:10:20 +0800

    Implement sys.stdout methods

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import datetime
 from time import sleep
 
-from displayLED import *
+#from displayLED import *
 from mediaAPI import *
 from scheduleAPI import *
     
@@ -14,6 +14,7 @@ def main():
     
 	while True:
 		current_time = datetime.datetime.now().strftime("%H:%M")
+		print_to_screen("Current Time: " + current_time)
         
 		if timing_exists(current_time, schedule):
 			"""
@@ -23,15 +24,16 @@ def main():
 			if activity_alert:
 				activity_alert = 0
 				print_timing_activity(current_time, schedule[current_time])
-				displayLED(0)
+				#displayLED(0)
 				play_audio('./sounds/activity.wav')
 				# TODO: Add in physical action
 				# TODO: Insert while loop for game here.
 				# TODO: Add in noise and action if activity is not completed.
+				
                 
 		else:
 			"""
-			If there is no activity, the robot acts like a clock with hourly updates.
+			If there is no activity, the robot acts like a clock with hourly actions.
 			"""
 			
 			activity_alert = 1
@@ -39,14 +41,16 @@ def main():
 			if current_time[3:5] == '00':  # if it is a full hour timing
 				if hourly_alert:
 					hourly_alert = 0
-					displayLED(3)
+					#displayLED(3)
 					play_audio('./sounds/hourly.wav')
 					# TODO: Add in physical action
-					print_timing(current_time)
 			else:
 				hourly_alert = 1
-				displayLED(2)
+				#displayLED(2)
 				play_audio('./sounds/angry.wav')
+				
+			sleep(1)
+			clear_screen_text("Current Time: " + current_time)
 
         
 if __name__ == "__main__":
