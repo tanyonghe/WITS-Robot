1cd7f8eb4f36c44bd4653a89ed988345da28f0fe
x3tsunayh <tyhshana@hotmail.com>
2018-12-30 14:07:32 +0800

    Update required dependencies

--- setup.sh
+++ setup.sh
@@ -3,10 +3,20 @@
 sudo apt-get install build-essential
 sudo apt-get install python3-dev python3-pip
 sudo apt-get install libfreetype6-dev libjpeg-dev
+
+sudo apt-get install libcblas-dev
+sudo apt-get install libhdf5-dev
+sudo apt-get install libhdf5-serial-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libjasper-dev 
+sudo apt-get install libqtgui4 
+sudo apt-get install libqt4-test
+
+sudo pip3 install opencv-python
+sudo pip3 simpleaudio
+
 git clone http://github.com/rm-hull/luma.led_matrix.git
 sudo -H pip3 install luma.led_matrix
-
-sudo pip3 simpleaudio
 
 chmod 755 main.py
 chmod 755 demo.py
