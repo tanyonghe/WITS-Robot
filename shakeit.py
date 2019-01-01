7467736dfa77618a3eefc60630c5e9900003056a
x3tsunayh <tyhshana@hotmail.com>
2019-01-01 18:35:41 +0800

    Touched up game gui

--- shakeit.py
+++ shakeit.py
@@ -16,7 +16,7 @@ class DevNull:
     def write(self, msg):
         pass
 
-#sys.stderr = DevNull()  # to squash errors for the time being
+sys.stderr = DevNull()  # to squash errors for the time being
 
 
 FPS = 30
@@ -58,16 +58,26 @@ def retrieve_UDP_values():
 		
 def showScore(score, ratio = 0.1):
 	"""displays score in center of screen"""
-	scoreDigits = [int(x) for x in list(str(score))]
+	scoreDigits = list(str(score))
 	totalWidth = 0 # total width of all numbers to be printed
 
 	for digit in scoreDigits:
+		if digit == '.':
+			digit = 10
+		else:
+			digit = int(digit)
 		totalWidth += IMAGES['numbers'][digit].get_width()
 
 	Xoffset = (SCREENWIDTH - totalWidth) / 2
 
 	for digit in scoreDigits:
-		SCREEN.blit(IMAGES['numbers'][digit], (Xoffset, SCREENHEIGHT * ratio))
+		if digit == '.':
+			digit = 10
+			scaled_dot = pygame.transform.scale(IMAGES['numbers'][digit], (10, 10))
+			SCREEN.blit(scaled_dot, (Xoffset + 7, SCREENHEIGHT * (ratio + 0.07)))
+		else:
+			digit = int(digit)
+			SCREEN.blit(IMAGES['numbers'][digit], (Xoffset, SCREENHEIGHT * ratio))
 		Xoffset += IMAGES['numbers'][digit].get_width()
 
 		
@@ -76,7 +86,7 @@ def main():
 	pygame.init()
 	FPSCLOCK = pygame.time.Clock()
 	SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
-	pygame.display.set_caption('Flappy Bird')
+	pygame.display.set_caption('Shake It')
 
     # numbers sprites for score display
 	IMAGES['numbers'] = (
@@ -89,15 +99,22 @@ def main():
 		pygame.image.load('assets/sprites/6.png').convert_alpha(),
 		pygame.image.load('assets/sprites/7.png').convert_alpha(),
 		pygame.image.load('assets/sprites/8.png').convert_alpha(),
-		pygame.image.load('assets/sprites/9.png').convert_alpha()
+		pygame.image.load('assets/sprites/9.png').convert_alpha(),
+		pygame.image.load('assets/sprites/dot.png').convert_alpha()
 	)
 	
 	# game over sprite
 	IMAGES['gameover'] = pygame.image.load('assets/sprites/gameover.png').convert_alpha()
+	IMAGES['timetaken'] = pygame.image.load('assets/sprites/timetaken.png').convert_alpha()
+	IMAGES['seconds'] = pygame.image.load('assets/sprites/seconds.png').convert_alpha()
 	# message sprite for welcome screen
-	IMAGES['message'] = pygame.image.load('assets/sprites/message.png').convert_alpha()
+	IMAGES['shakeit'] = pygame.image.load('assets/sprites/shakeit.png').convert_alpha()
+	IMAGES['team14'] = pygame.image.load('assets/sprites/team14.png').convert_alpha()
+	IMAGES['countdown'] = pygame.image.load('assets/sprites/countdown.png').convert_alpha()
 	# base (ground) sprite
 	IMAGES['base'] = pygame.image.load('assets/sprites/base.png').convert_alpha()
+	# game animation sprite
+	IMAGES['phoneshake'] = pygame.image.load('assets/sprites/phoneshake.png').convert_alpha()
 
 	while True:
 		# select random background sprites
@@ -110,30 +127,47 @@ def main():
 
 def showWelcomeAnimation():
 
+	tapped = False
+	
+	shakeitx = int((SCREENWIDTH - IMAGES['shakeit'].get_width()) / 2)
+	shakeity = int(SCREENHEIGHT * 0.12)
+	team14x = int((SCREENWIDTH - IMAGES['team14'].get_width()) / 2)
+	team14y = int(SCREENHEIGHT * 0.45)
+	countdownx = int((SCREENWIDTH - IMAGES['countdown'].get_width()) / 2)
+	countdowny = int(SCREENHEIGHT * 0.35)
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
 				pygame.quit()
 				#sys.exit()
 			if event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
-				if playery + playerHeight >= BASEY - 1:
-					return
+				tapped = True
 	
 		SCREEN.blit(IMAGES['background'], (0,0))
+		SCREEN.blit(IMAGES['shakeit'], (shakeitx, shakeity))
+		SCREEN.blit(IMAGES['team14'], (team14x, team14y))
 		A_XYZ = retrieve_UDP_values()
 		
-		if A_XYZ > 2:
-			showScore(3, 0.4)
+		if A_XYZ > 2 or tapped:
+			SCREEN.blit(IMAGES['background'], (0,0))
+			SCREEN.blit(IMAGES['shakeit'], (shakeitx, shakeity))
+			SCREEN.blit(IMAGES['countdown'], (countdownx, countdowny))
+			showScore(3, 0.45)
 			pygame.display.update()
 			FPSCLOCK.tick(FPS)
 			time.sleep(1)
 			SCREEN.blit(IMAGES['background'], (0,0))
-			showScore(2, 0.4)
+			SCREEN.blit(IMAGES['shakeit'], (shakeitx, shakeity))
+			SCREEN.blit(IMAGES['countdown'], (countdownx, countdowny))
+			showScore(2, 0.45)
 			pygame.display.update()
 			FPSCLOCK.tick(FPS)
 			time.sleep(1)
 			SCREEN.blit(IMAGES['background'], (0,0))
-			showScore(1, 0.4)
+			SCREEN.blit(IMAGES['shakeit'], (shakeitx, shakeity))
+			SCREEN.blit(IMAGES['countdown'], (countdownx, countdowny))
+			showScore(1, 0.45)
 			pygame.display.update()
 			FPSCLOCK.tick(FPS)
 			time.sleep(1)
@@ -144,7 +178,10 @@ def showWelcomeAnimation():
 
 def mainGame():
 	
-	score = 50
+	tapped = False
+	shake = 1
+	
+	score = 0
 	start_time = time.time()
 	
 	while True:
@@ -153,24 +190,22 @@ def mainGame():
 				pygame.quit()
 				#sys.exit()
 			if event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
-				if playery + playerHeight >= BASEY - 1:
-					return	
+				tapped = True	
 	
 		# draw sprites
 		SCREEN.blit(IMAGES['background'], (0,0))
-		#SCREEN.blit(IMAGES['player'][playerIndex], (playerx, playery + playerShmVals['val']))
-		#SCREEN.blit(IMAGES['message'], (messagex, messagey))
-		#SCREEN.blit(IMAGES['base'], (basex, BASEY)
-		
 		
 		A_XYZ = retrieve_UDP_values()
 		
-		if A_XYZ > 2:
+		if A_XYZ > 2 or tapped:
+			shake = -shake
 			score += 1
 			if score == 100:
 				return time.time() - start_time
 				
 		showScore(score)
+		shaker = pygame.transform.rotate(IMAGES['phoneshake'], shake * 15)
+		SCREEN.blit(shaker, (30,100))
 
 		pygame.display.update()
 		FPSCLOCK.tick(FPS)
@@ -179,7 +214,9 @@ def mainGame():
 	
 
 def showGameOverScreen(elapsed_time):
-	truncated_time = str(math.ceil(elapsed_time * 100.0) / 100.0).split('.')
+
+	tapped = False
+	truncated_time = str(math.ceil(elapsed_time * 1000.0) / 1000.0)
 	reset = 0
 
 	while True:
@@ -188,25 +225,25 @@ def showGameOverScreen(elapsed_time):
 				pygame.quit()
 				#sys.exit()
 			if event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
-				if playery + playerHeight >= BASEY - 1:
-					return
+				tapped = True
 		
 		A_XYZ = retrieve_UDP_values()
 		
-		if A_XYZ > 2:
-			return
+		if A_XYZ > 2 or tapped:
+			reset += 1
+			if reset == 10:
+				return
 		
 		# draw sprites
-		SCREEN.blit(IMAGES['background'], (0,0))
-		SCREEN.blit(IMAGES['gameover'], (50,75))
-		showScore(truncated_time[0], 0.4)
+		SCREEN.blit(IMAGES['background'], (0, 0))
+		SCREEN.blit(IMAGES['gameover'], (50, 75))
+		SCREEN.blit(IMAGES['timetaken'], ((SCREENWIDTH - IMAGES['timetaken'].get_width()) / 2, SCREENHEIGHT * 0.42))
+		showScore(truncated_time, 0.5)
+		SCREEN.blit(IMAGES['seconds'], ((SCREENWIDTH - IMAGES['seconds'].get_width()) / 2, SCREENHEIGHT * 0.62))
 		
 		FPSCLOCK.tick(FPS)
 		pygame.display.update()
 
-	# Welcome Screen
-	# Loading Bar Graphics
-	# "Time Taken" Text, "Game starts in", ".", "seconds", "Shake to restart game"
 	
 	
 if __name__ == '__main__':
